ff3ae1a6e3a81a24e55653f040457abd122f762b
biwer-r <rene.biwer@siegenia.com>
2016-06-04 13:31:32 +0200

    removed columns classes fields in row

--- dca/tl_content.php
+++ dca/tl_content.php
@@ -21,32 +21,9 @@ if (TL_MODE === 'BE') {
 
 $GLOBALS['TL_DCA']['tl_content']['config']['onsubmit_callback'][] = array('MadeYourDay\\Contao\\Columns', 'onsubmitCallback');
 
-$GLOBALS['TL_DCA']['tl_content']['palettes']['rs_columns_start'] = '{type_legend},type,headline;{rs_columns_legend},rs_columns_large,rs_columns_medium,rs_columns_small;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['rs_columns_start'] = '{type_legend},type,headline;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
 $GLOBALS['TL_DCA']['tl_content']['palettes']['rs_columns_stop'] = '{type_legend},type;{protected_legend:hide},protected;{expert_legend:hide},guests;{invisible_legend:hide},invisible,start,stop';
 $GLOBALS['TL_DCA']['tl_content']['palettes']['rs_column_start'] = '{type_legend},type,headline;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
 $GLOBALS['TL_DCA']['tl_content']['palettes']['rs_column_stop'] = '{type_legend},type;{protected_legend:hide},protected;{expert_legend:hide},guests;{invisible_legend:hide},invisible,start,stop';
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['rs_columns_large'] = array(
-	'inputType' => 'text',
-	'label' => &$GLOBALS['TL_LANG']['tl_content']['rs_columns_large'],
-	'eval' => array(
-		'tl_class' => 'rs_columns_w33',
-	),
-	'sql' => "varchar(255) NOT NULL default ''",
-);
-$GLOBALS['TL_DCA']['tl_content']['fields']['rs_columns_medium'] = array(
-	'inputType' => 'text',
-	'label' => &$GLOBALS['TL_LANG']['tl_content']['rs_columns_medium'],
-	'eval' => array(
-		'tl_class' => 'rs_columns_w33',
-	),
-	'sql' => "varchar(255) NOT NULL default ''",
-);
-$GLOBALS['TL_DCA']['tl_content']['fields']['rs_columns_small'] = array(
-	'inputType' => 'text',
-	'label' => &$GLOBALS['TL_LANG']['tl_content']['rs_columns_small'],
-	'eval' => array(
-		'tl_class' => 'rs_columns_w33',
-	),
-	'sql' => "varchar(255) NOT NULL default ''",
-);
+
